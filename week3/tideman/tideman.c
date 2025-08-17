d5c894d9a9147ab8639cdfbcf1a47bda9308eb18
Divine Benjamin <divinebenjamin2004@gmail.com>
2025-08-17 22:10:26 +0100

    tideman | finished sort_pairs func

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -156,7 +156,31 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    // TODO
+    // TODO    
+    for(int i = 0; i < pair_count - 1; i++)
+    {
+        int max = preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner];
+        int current_max;
+        int max_index = i;
+        
+        for(int j = i + 1; j < pair_count; j++)
+        {
+            current_max = preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner];
+
+            if(max < current_max)
+            {
+                max = current_max;
+                max_index = j;
+            }
+        }
+        if(max_index != i)
+        {
+            pair temp = pairs[i];
+            pairs[i] = pairs[max_index];
+            pairs[max_index] = temp;
+        }
+    }
+
     return;
 }
 
@@ -164,6 +188,15 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // TODO
+    for(int i = 0; i < pair_count; i++)
+    {
+        if(not_cycle)
+        {
+            locked[pairs[i].winner][pairs[i].loser];
+        }
+    }
+
+
     return;
 }
 
@@ -173,3 +206,4 @@ void print_winner(void)
     // TODO
     return;
 }
+
