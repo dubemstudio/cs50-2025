b16c58ac240de2987a04fbc1dcbc1d8b4e7b09b0
Divine Benjamin <divinebenjamin2004@gmail.com>
2025-09-06 19:35:47 +0100

    filter-less | finished greyscale

---
+++ week4/filter-less/helpers.c
@@ -0,0 +1,40 @@
+#include "helpers.h"
+#include <math.h>
+
+// Convert image to grayscale
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+
+            // Get average
+            int average = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0 );
+
+            // Update pixel
+            image[i][j].rgbtRed = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtBlue = average;
+
+        }
+    }
+    return;
+}
+
+// Convert image to sepia
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Reflect image horizontally
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
+
+// Blur image
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
+    return;
+}
