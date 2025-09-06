bccba6e58a751b9a59c023d8888a5b1c5ba5d162
Divine Benjamin <divinebenjamin2004@gmail.com>
2025-09-06 19:57:51 +0100

    filter-less | finished sepia

--- week4/filter-less/helpers.c
+++ week4/filter-less/helpers.c
@@ -24,6 +24,21 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+
+            // ALgorithm provided
+            int sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 0.189 * image[i][j].rgbtBlue);
+            int sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 0.168 * image[i][j].rgbtBlue);
+            int sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 0.131 * image[i][j].rgbtBlue);
+
+            // Update pixel
+            image[i][j].rgbtRed = (sepiaRed > 255) ? 255 : sepiaRed;
+            image[i][j].rgbtGreen = (sepiaGreen > 255) ? 255 : sepiaGreen;
+            image[i][j].rgbtBlue = (sepiaBlue > 255) ? 255 : sepiaBlue;
+        }
+    }
     return;
 }
 
